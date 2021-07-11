dca6e669b1ecbb848579235f9d699a403bbadd67
Rahul <r27m93@gmail.com>
2021-07-11 14:06:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Tilty from 'react-tilty'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Tilty
+        className='Tilt'
+        options={{ max: 25 }}
+        style={{ height: 250, width: 250 }}
+        speed={400}
+        glare={true}
+        max-glare={1}
+      >
+        <div className='container'>
+          <div className='card'>
+            <h1>Hello World</h1>
+          </div>
+        </div>
+      </Tilty>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
